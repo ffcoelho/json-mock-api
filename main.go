24283d02284b442a6a7e001eec2518b6ce0138bb
ffcoelho <ffcoelho.dev@gmail.com>
2022-12-28 15:48:45 -0300

    add exit handler

--- main.go
+++ main.go
@@ -3,14 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	setupExitHandler()
 	http.HandleFunc("/", handler)
-	fmt.Printf("Listening on http://localhost:%d\n", 9000)
+	fmt.Printf("JSON Mock API is up.\nListening on http://localhost:%d\n", 9000)
 	http.ListenAndServe(":9000", nil)
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("Ok"))
+}
+
+func setupExitHandler() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("\rJSON Mock API is down.")
+		os.Exit(0)
+	}()
 }
